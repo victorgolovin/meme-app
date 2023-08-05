9af6892312b1bb53e146b3985cd2de5431d9d3b7
unknown <rinoally@gmail.com>
2023-08-05 16:30:09 +0300

    create JS

---
+++ js/view.js
@@ -0,0 +1,8 @@
+class View {
+    constructor() {
+        this.memeInputNode = document.getElementById('settings-meme-input');
+        this.aboveInputNode = document.getElementById('settings-text-above-input');
+        this.belowInputNode = document.getElementById('settings-text-below-input');
+        this.outputNode = document.getElementById('preview-output');
+    }
+}
